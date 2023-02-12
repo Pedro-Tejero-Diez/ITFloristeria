20c9b0d8cf09d40ccce53fd1a225b1b73b7712f8
pedrete3000 <pedrete3000@hotmail.com>
2023-02-12 12:48:27 +0100

    casa 12 febrero

--- TestConnection/src/pruebinas/TestConnection.java
+++ TestConnection/src/pruebinas/TestConnection.java
@@ -10,34 +10,33 @@ import java.sql.Driver;
 public class TestConnection {
 
 	public static void main(String[] args) throws SQLException {
-		 Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		try {
-	            // The newInstance() call is a work around for some
-	            // broken Java implementations
+			// The newInstance() call is a work around for some
+			// broken Java implementations
 
-	            Class.forName("com.mysql.cj.jdbc.Driver");
-	        } catch (Exception ex) {
-	            // handle the error
-	        }
-	
-		
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (Exception ex) {
+			// handle the error
+		}
+
 		Connection con = null;
 		System.out.println("introduce usuario base de datos: ");
 		String username = sc.nextLine();
 		System.out.println("introduce password base de datos: ");
-        String password = sc.nextLine();
+		String password = sc.nextLine();
 
+		con = DriverManager.getConnection("jdbc:mysql://localhost:3306/floristeria", username, password);
 
-con = DriverManager.getConnection(
-		"jdbc:mysql://localhost:3306/restaurant", username, password);
+		System.out.println("Connected to database");
 
-System.out.println("Connected to database");
-
-java.sql.Statement st = con.createStatement();
-ResultSet mar = st.executeQuery("SELECT * FROM categoria");
-System.out.println(mar);
+		java.sql.Statement st = con.createStatement();
+		ResultSet mar = st.executeQuery("SELECT altura, precio FROM stockarbol");
+		while (mar.next()) {
+			float alt = mar.getFloat("altura");
+			float prc = mar.getFloat("precio");
+			System.out.println(alt + "                 " + prc);
 		}
-
-
 	}
 
+}
