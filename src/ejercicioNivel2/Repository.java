90526c0d7d50c59751b5b7e87e13e794ab166fc2
pedrete3000 <pedrete3000@hotmail.com>
2023-02-17 12:29:40 +0100

    cambios dia 16 sinergics

--- src/ejercicioNivel2/Repository.java
+++ src/ejercicioNivel2/Repository.java
@@ -9,14 +9,12 @@ import java.util.Scanner;
 import com.mysql.cj.protocol.Resultset;
 
 public class Repository {
-	
 
 	static Scanner sc = new Scanner(System.in);
 	static Connection con = null;
-	
+
 	public static Connection nuevaConexion() {
 
-		
 		System.out.println("introduce usuario base de datos: ");
 		String username = sc.nextLine();
 		System.out.println("introduce password base de datos: ");
@@ -53,7 +51,7 @@ public class Repository {
 		String tree = "INSERT INTO stockarbol (altura,precio,cantidad)" + "VALUES (" + altura + "," + precio + ","
 				+ numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(tree);
@@ -65,7 +63,7 @@ public class Repository {
 		String flower = "INSERT INTO stockflor (color,precio,cantidad)" + "VALUES (" + "'" + color + "'" + "," + precio
 				+ "," + numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(flower);
@@ -77,7 +75,7 @@ public class Repository {
 		String decor = "INSERT INTO stockdecor (tipo,precio,cantidad)" + "VALUES (" + "'" + tipo + "'" + "," + precio
 				+ "," + numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(decor);
@@ -90,91 +88,94 @@ public class Repository {
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenArbol = st.executeQuery("SELECT * FROM stockarbol");
-		return  almacenArbol;
-	
+		return almacenArbol;
+
 	}
 
-public static ResultSet bringStockFlor (String name) throws SQLException {
+	public static ResultSet bringStockFlor(String name) throws SQLException {
 		Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenFlor = st.executeQuery("SELECT * FROM stockflor");
-		return almacenFlor; 
-}
-			
+		return almacenFlor;
+	}
+
 	public static ResultSet bringStockDecor(String name) throws SQLException {
 		Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenDecor = st.executeQuery("SELECT * FROM stockdecor");
 		return almacenDecor;
-	
+
 	}
-	
+
 	public static void restarArbol(String name, int numero, float altura) throws SQLException {
 		ResultSet sA = bringStockArbol(name);
-		int qt =0;
-		 while (sA.next() && qt==0) { 
-			  float alt = sA.getFloat("altura");
-			 if (alt==altura) {
-				  qt = sA.getInt("cantidad");
-			 }
-		 }  
-		 int diferencia = qt - numero;
-		 if (diferencia<0) {
-			 System.out.println("La cantidad elegida es superior al stock");
-		 } else {
-		 Statement st = con.createStatement();
+		int qt = 0;
+		while (sA.next() && qt == 0) {
+			float alt = sA.getFloat("altura");
+			if (alt == altura) {
+				qt = sA.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
 			st.execute("USE " + name);
-			st.executeUpdate("UPDATE stockarbol SET cantidad = "
-			+diferencia+ " WHERE (altura = "+altura+");");
-		 }
+			st.executeUpdate("UPDATE stockarbol SET cantidad = " + diferencia + " WHERE (altura = " + altura + ");");
+		}
 	}
+
 	public static void restarFlor(String name, int numero, String color) throws SQLException {
 		ResultSet sF = bringStockFlor(name);
-		int qt =0;
-		 while (sF.next() && qt==0) { 
-			  String col = sF.getString("color");
-			 if (col.equalsIgnoreCase(color)) {
-				  qt = sF.getInt("cantidad");
-			 }
-		 }  
-		 int diferencia = qt - numero;
-		 if (diferencia<0) {
-			 System.out.println("La cantidad elegida es superior al stock");
-		 } else {
-		 Statement st = con.createStatement();
+		int qt = 0;
+		while (sF.next() && qt == 0) {
+			String col = sF.getString("color");
+			if (col.equalsIgnoreCase(color)) {
+				qt = sF.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
 			st.execute("USE " + name);
-			st.executeUpdate("UPDATE stockflor SET cantidad = "
-			+diferencia+ " WHERE (color = "+ "'" + color + "'" +");");
-		 }
+			st.executeUpdate(
+					"UPDATE stockflor SET cantidad = " + diferencia + " WHERE (color = " + "'" + color + "'" + ");");
+		}
 	}
-		 public static void restarDecoracion(String name, int numero, String tipo) throws SQLException {
-				ResultSet sD = bringStockDecor(name);
-				int qt =0;
-				 while (sD.next() && qt==0) { 
-					  String tip = sD.getString("tipo");
-					 if (tip.equalsIgnoreCase(tipo)) {
-						  qt = sD.getInt("cantidad");
-					 }
-				 }  
-				 int diferencia = qt - numero;
-				 if (diferencia<0) {
-					 System.out.println("La cantidad elegida es superior al stock");
-				 } else {
-				 Statement st = con.createStatement();
-					st.execute("USE " + name);
-					st.executeUpdate("UPDATE stockdecor SET cantidad = "
-					+diferencia+ " WHERE (tipo = "+ "'" + tipo + "'" +");");
-				 }
-}
-		 public static ResultSet mostrarCantidades (String name) throws SQLException {
-			 Connection con = nuevaConexion();
-				Statement st = con.createStatement();
-				st.execute("USE " + name);
-				ResultSet totales = st.executeQuery("SELECT stockarbol.cantidad, stockarbol.altura, stockflor.color, stockflor.cantidad, ,"
+
+	public static void restarDecoracion(String name, int numero, String tipo) throws SQLException {
+		ResultSet sD = bringStockDecor(name);
+		int qt = 0;
+		while (sD.next() && qt == 0) {
+			String tip = sD.getString("tipo");
+			if (tip.equalsIgnoreCase(tipo)) {
+				qt = sD.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
+			st.execute("USE " + name);
+			st.executeUpdate(
+					"UPDATE stockdecor SET cantidad = " + diferencia + " WHERE (tipo = " + "'" + tipo + "'" + ");");
+		}
+	}
+
+	public static ResultSet mostrarCantidades(String name) throws SQLException {
+		Connection con = nuevaConexion();
+		Statement st = con.createStatement();
+		st.execute("USE " + name);
+		ResultSet totales = st
+				.executeQuery("SELECT stockarbol.cantidad, stockarbol.altura, stockflor.color, stockflor.cantidad, ,"
 						+ "stockdecor.tipo, stockdecor.cantidad FROM stockarbol, stockflor, stockdecor");
-				return totales;
-			 
-		 }
+		return totales;
+
+	}
 }
