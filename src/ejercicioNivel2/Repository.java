7ce4ce5a7eb42a4db5b16b0eea3f73dc33032e02
pedrete3000 <pedrete3000@hotmail.com>
2023-02-17 07:22:10 +0100

    ejercicio actualizado 17 de febrero

--- src/ejercicioNivel2/Repository.java
+++ src/ejercicioNivel2/Repository.java
@@ -5,18 +5,18 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import com.mysql.cj.protocol.Resultset;
 
 public class Repository {
-	
 
 	static Scanner sc = new Scanner(System.in);
 	static Connection con = null;
-	
+
 	public static Connection nuevaConexion() {
 
-		
 		System.out.println("introduce usuario base de datos: ");
 		String username = sc.nextLine();
 		System.out.println("introduce password base de datos: ");
@@ -53,7 +53,7 @@ public class Repository {
 		String tree = "INSERT INTO stockarbol (altura,precio,cantidad)" + "VALUES (" + altura + "," + precio + ","
 				+ numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(tree);
@@ -65,7 +65,7 @@ public class Repository {
 		String flower = "INSERT INTO stockflor (color,precio,cantidad)" + "VALUES (" + "'" + color + "'" + "," + precio
 				+ "," + numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(flower);
@@ -77,7 +77,7 @@ public class Repository {
 		String decor = "INSERT INTO stockdecor (tipo,precio,cantidad)" + "VALUES (" + "'" + tipo + "'" + "," + precio
 				+ "," + numero + ");";
 
-		//Connection con = nuevaConexion();
+		// Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		st.executeUpdate(decor);
@@ -90,91 +90,154 @@ public class Repository {
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenArbol = st.executeQuery("SELECT * FROM stockarbol");
-		return  almacenArbol;
-	
+		return almacenArbol;
+
 	}
 
-public static ResultSet bringStockFlor (String name) throws SQLException {
+	public static ResultSet bringStockFlor(String name) throws SQLException {
 		Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenFlor = st.executeQuery("SELECT * FROM stockflor");
-		return almacenFlor; 
-}
-			
+		return almacenFlor;
+	}
+
 	public static ResultSet bringStockDecor(String name) throws SQLException {
 		Connection con = nuevaConexion();
 		Statement st = con.createStatement();
 		st.execute("USE " + name);
 		ResultSet almacenDecor = st.executeQuery("SELECT * FROM stockdecor");
 		return almacenDecor;
-	
+
 	}
-	
+
 	public static void restarArbol(String name, int numero, float altura) throws SQLException {
 		ResultSet sA = bringStockArbol(name);
-		int qt =0;
-		 while (sA.next() && qt==0) { 
-			  float alt = sA.getFloat("altura");
-			 if (alt==altura) {
-				  qt = sA.getInt("cantidad");
-			 }
-		 }  
-		 int diferencia = qt - numero;
-		 if (diferencia<0) {
-			 System.out.println("La cantidad elegida es superior al stock");
-		 } else {
-		 Statement st = con.createStatement();
+		int qt = 0;
+		while (sA.next() && qt == 0) {
+			float alt = sA.getFloat("altura");
+			if (alt == altura) {
+				qt = sA.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
 			st.execute("USE " + name);
-			st.executeUpdate("UPDATE stockarbol SET cantidad = "
-			+diferencia+ " WHERE (altura = "+altura+");");
-		 }
+			st.executeUpdate("UPDATE stockarbol SET cantidad = " + diferencia + " WHERE (altura = " + altura + ");");
+		}
 	}
+
 	public static void restarFlor(String name, int numero, String color) throws SQLException {
 		ResultSet sF = bringStockFlor(name);
-		int qt =0;
-		 while (sF.next() && qt==0) { 
-			  String col = sF.getString("color");
-			 if (col.equalsIgnoreCase(color)) {
-				  qt = sF.getInt("cantidad");
-			 }
-		 }  
-		 int diferencia = qt - numero;
-		 if (diferencia<0) {
-			 System.out.println("La cantidad elegida es superior al stock");
-		 } else {
-		 Statement st = con.createStatement();
+		int qt = 0;
+		while (sF.next() && qt == 0) {
+			String col = sF.getString("color");
+			if (col.equalsIgnoreCase(color)) {
+				qt = sF.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
 			st.execute("USE " + name);
-			st.executeUpdate("UPDATE stockflor SET cantidad = "
-			+diferencia+ " WHERE (color = "+ "'" + color + "'" +");");
-		 }
+			st.executeUpdate(
+					"UPDATE stockflor SET cantidad = " + diferencia + " WHERE (color = " + "'" + color + "'" + ");");
+		}
 	}
-		 public static void restarDecoracion(String name, int numero, String tipo) throws SQLException {
-				ResultSet sD = bringStockDecor(name);
-				int qt =0;
-				 while (sD.next() && qt==0) { 
-					  String tip = sD.getString("tipo");
-					 if (tip.equalsIgnoreCase(tipo)) {
-						  qt = sD.getInt("cantidad");
-					 }
-				 }  
-				 int diferencia = qt - numero;
-				 if (diferencia<0) {
-					 System.out.println("La cantidad elegida es superior al stock");
-				 } else {
-				 Statement st = con.createStatement();
-					st.execute("USE " + name);
-					st.executeUpdate("UPDATE stockdecor SET cantidad = "
-					+diferencia+ " WHERE (tipo = "+ "'" + tipo + "'" +");");
-				 }
-}
-		 public static ResultSet mostrarCantidades (String name) throws SQLException {
-			 Connection con = nuevaConexion();
-				Statement st = con.createStatement();
-				st.execute("USE " + name);
-				ResultSet totales = st.executeQuery("SELECT stockarbol.cantidad, stockarbol.altura, stockflor.color, stockflor.cantidad, ,"
-						+ "stockdecor.tipo, stockdecor.cantidad FROM stockarbol, stockflor, stockdecor");
-				return totales;
-			 
-		 }
+
+	public static void restarDecoracion(String name, int numero, String tipo) throws SQLException {
+		ResultSet sD = bringStockDecor(name);
+		int qt = 0;
+		while (sD.next() && qt == 0) {
+			String tip = sD.getString("tipo");
+			if (tip.equalsIgnoreCase(tipo)) {
+				qt = sD.getInt("cantidad");
+			}
+		}
+		int diferencia = qt - numero;
+		if (diferencia < 0) {
+			System.out.println("La cantidad elegida es superior al stock");
+		} else {
+			Statement st = con.createStatement();
+			st.execute("USE " + name);
+			st.executeUpdate(
+					"UPDATE stockdecor SET cantidad = " + diferencia + " WHERE (tipo = " + "'" + tipo + "'" + ");");
+		}
+	}
+
+	public static ResultSet mostrarCantidadArbol(String name) throws SQLException {
+		Connection con = nuevaConexion();
+		Statement st = con.createStatement();
+		st.execute("USE " + name);
+		ResultSet totales = st
+				.executeQuery("SELECT stockarbol.cantidad, stockarbol.altura, stockarbol.precio" + " FROM stockarbol;");
+		return totales;
+	}
+
+	public static ResultSet mostrarCantidadFlor(String name) throws SQLException {
+		Connection con = nuevaConexion();
+		Statement st = con.createStatement();
+		st.execute("USE " + name);
+		ResultSet totales = st
+				.executeQuery("SELECT stockflor.cantidad, stockflor.color, stockflor.precio FROM stockflor;");
+		return totales;
+	}
+
+	public static ResultSet mostrarCantidadDecor(String name) throws SQLException {
+		Connection con = nuevaConexion();
+		Statement st = con.createStatement();
+		st.execute("USE " + name);
+		ResultSet totales = st
+				.executeQuery("SELECT stockdecor.cantidad, stockdecor.tipo, stockdecor.precio FROM stockdecor;");
+		return totales;
+	}
+
+	public static void crearTicketCompra(String name, int arbol, int arboles, String color, int flores, String tipo,
+			int decoraciones) throws SQLException {
+		String fecha = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date());
+		Connection con = nuevaConexion();
+		Statement st = con.createStatement();
+		st.execute("USE " + name);
+		st.execute("CREATE TABLE IF NOT EXISTS ticket (\r\n"
+				+ "  `ticket_id` INT NOT NULL AUTO_INCREMENT ,"
+				+ "  `fecha` DATETIME NOT NULL ,"
+				+ "  `tipo_arbol` INT NULL,"
+				+ "  `cantidad_arbol` INT NULL,"
+				+ "  `tipo_flor` INT NULL,"
+				+ "  `cantidad_flor` INT NULL,"
+				+ "  `tipo_decor` INT NULL,"
+				+ "  `cantidad_decor` INT NULL,"
+				+ "  PRIMARY KEY (`ticket_id`),"
+				+ "  UNIQUE INDEX `ticket_id_UNIQUE` (`ticket_id` ASC) VISIBLE,"
+				+ "  INDEX `tipo_arbol_idx` (`tipo_arbol` ASC) VISIBLE,"
+				+ "  INDEX `tipo_flor_idx` (`tipo_flor` ASC) VISIBLE,"
+				+ "  INDEX `tipo_decor_idx` (`tipo_decor` ASC) VISIBLE,"
+				+ "  CONSTRAINT `tipo_arbol`"
+				+ "    FOREIGN KEY (`tipo_arbol`)"
+				+ "    REFERENCES `juliancito`.`stockarbol` (`idstockarbol`)"
+				+ "    ON DELETE NO ACTION"
+				+ "    ON UPDATE NO ACTION,"
+				+ "  CONSTRAINT `tipo_flor`"
+				+ "    FOREIGN KEY (`tipo_flor`)"
+				+ "    REFERENCES `juliancito`.`stockflor` (`flor_id`)"
+				+ "    ON DELETE NO ACTION"
+				+ "    ON UPDATE NO ACTION,"
+				+ "  CONSTRAINT `tipo_decor`"
+				+ "    FOREIGN KEY (`tipo_decor`)"
+				+ "    REFERENCES `juliancito`.`stockdecor` (`decor_id`)"
+				+ "	ON DELETE NO ACTION"
+				+ "    ON UPDATE NO ACTION);");
+		st.executeUpdate("INSERT INTO ticket (fecha, tipo_arbol, cantidad_arbol, tipo_flor, cantidad_flor, tipo_decor, cantidad_decor) VALUES ('"+fecha+"' ,"+arbol+", "+arboles+","+color+","+flores+","+tipo+","+decoraciones+");");
+
+		restarArbol(name, arbol, arboles);
+		restarFlor(name, flores, color);
+		restarDecoracion(name, decoraciones, tipo);
+
+		
+	}
 }
--- src/ejercicioNivel2/VistaFloristeria.java
+++ src/ejercicioNivel2/VistaFloristeria.java
@@ -55,52 +55,56 @@ public class VistaFloristeria {
 		}
 
 
-	public void mostrarValorTotal(String a, String b, String c) throws FileNotFoundException, IOException {
-		double valorArboles = (double) a.length() * arbol.getPrecio();
-		double valorFlores = (double) b.length() * flor.getPrecio();
-		double valorDecoracion = (double) c.length() * arbol.getPrecio();
+	public void mostrarValorTotal(String name) throws SQLException {
 
-		System.out.println("Valor Arboles = " + valorArboles);
-		System.out.println("Valor Flores = " + valorFlores);
-		System.out.println("Valor Decoraciones = " + valorDecoracion);
-		System.out.println("Valor Total Floristeria: " + (valorArboles + valorFlores + valorDecoracion));
+	float total=0;
+	ResultSet rA = Repository.mostrarCantidadArbol(name);
+	while (rA.next()) {
+		float qtpr= rA.getInt(1)*rA.getFloat(3);
+		
+		total += qtpr;
+	}
+	ResultSet rF = Repository.mostrarCantidadFlor(name);
+	while (rF.next()) {
+		float qtpr= rF.getInt(1)*rF.getFloat(3);
+		
+		total += qtpr;
+	}
+	ResultSet rD = Repository.mostrarCantidadDecor(name);
+	while (rD.next()) {
+		float qtpr= rD.getInt(1)*rD.getFloat(3);
+		
+		total += qtpr;
+	}
+	System.out.println("El total del stock en euros es: "+total);
 	}
 
 	public void mostrarCantidadesStock(String name) throws SQLException {
-	ResultSet rm = Repository.mostrarCantidades(name);
-	while(rm.next()) {
-		if (rm.getType().equals(double)) {
-			
-		}
-		
-		float alt = rm.getFloat("altura");
-		int qtt = rm.getInt("cantidad");
-		System.out.println(qtt);
-		
+	ResultSet rA = Repository.mostrarCantidadArbol(name);
+	int qt =0;
+	while (rA.next()) {
+		float alt = rA.getFloat(2);
+		qt = rA.getInt(1);
+		System.out.println("Altura árbol: "+alt+"        Cantidad: "+qt);
+	}
+	ResultSet rF = Repository.mostrarCantidadFlor(name);
+	while (rF.next()) {
+		String color = rF.getString(2);
+		qt = rF.getInt(1);
+		System.out.println("Color flor: "+color+"        Cantidad: "+qt);
+	}
+	ResultSet rD = Repository.mostrarCantidadDecor(name);
+	while (rD.next()) {
+		String tipo = rD.getString(2);
+		qt = rD.getInt(1);
+		System.out.println("Tipo Decoración: "+tipo+"        Cantidad: "+qt);
 	}
 	}
+	
 
-	public void imprimirTicketCompra(String name, int arboles, int flores, int decoraciones) {
-		double totalArboles = (double) arboles * arbol.getPrecio();
-		double totalFlores = (double) flores * flor.getPrecio();
-		double totalDecoraciones = (double) decoraciones * decoracion.getPrecio();
-		double totalTicket = totalArboles + totalFlores + totalDecoraciones;
-		sumatorioVentas(name, totalTicket);
-		String fecha = new SimpleDateFormat("yyyy-MM-dd__HH-mm").format(new Date());
-		try (FileWriter fw = new FileWriter(
-				"C:\\Users\\pedro\\eclipse-workspace\\S03T03N01PedroTejeroDiez\\lib\\src\\main\\java\\Floristeria "
-						+ name + "\\tickets\\ticket " + fecha + ".txt");
-				BufferedWriter bw = new BufferedWriter(fw);
-				PrintWriter out = new PrintWriter(bw)) {
-			out.println("FLORISTERIA:   " + name + "\n");
-			out.println("Fecha y hora de la compra:   " + fecha + "\n");
-			out.println("Arboles comprados: " + arboles + "         Precio: " + totalArboles + "\n");
-			out.println("Flores compradas: " + flores + "           Precio: " + totalFlores + "\n");
-			out.println("Decoraciones compradas: " + decoraciones + "     Precio: " + totalDecoraciones);
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-
+	public void imprimirTicketCompra(String name, float altura, int arboles, String color, int flores, String tipo, int decoraciones) {
+	
+		
 	}
 
 	public void sumatorioVentas(String name, double total) {
